1254df4c11d3af00077bc225bd792c0e14d84bbb
Rodolfo Charles <rosdolfo@gmail.com>
2025-11-25 17:51:44 -0300

    Configurações para logar o usuário

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,9 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  //console.log(req.session.user);
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
 };
 
 exports.register = async function (req, res) {
@@ -24,11 +26,44 @@ exports.register = async function (req, res) {
       });
       return;
     }
-    //return res.send(login.errors);
-  } catch (e) {
+   } catch (e) {
     console.log(e);
+    this.errors.push('Erro ao registrar usuário');
     res.render('404');
     return;
   }
-
 };
+
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        res.redirect('/login/index');
+        return;
+      });
+      return;
+    } else {
+      req.flash('success', 'Você acesso o sistema!');
+      req.session.user = login.user;
+      req.session.save(function () {
+        res.redirect('/login/index');
+        return;
+      });
+      return;
+    }
+   } catch (e) {
+    console.log(e);
+    this.errors.push('Erro ao registrar usuário');
+    res.render('404');
+    return;
+  }
+};
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+}
