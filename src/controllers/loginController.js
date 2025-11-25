ea255027698918b693894948bf890213090a72a2
Rodolfo Charles <rosdolfo@gmail.com>
2025-11-25 16:18:48 -0300

    Cadastro de usuário/login e validações

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,17 +4,31 @@ exports.index = (req, res) => {
   res.render('login');
 };
 
-exports.register = async function(req, res)  {
-  const login = new Login(req.body);
-  await login.register();
+exports.register = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
 
-  if (login.errors.length > 0) {
-    req.flash('errors', login.errors);
-    req.session.save(function() {
-      return res.redirect('/login/index');
-    });
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        res.redirect('/login/index');
+        return;
+      });
+      return;
+    } else {
+      req.flash('success', 'Seu usuário foi criado com sucesso!');
+      req.session.save(function () {
+        res.redirect('/login/index');
+        return;
+      });
+      return;
+    }
+    //return res.send(login.errors);
+  } catch (e) {
+    console.log(e);
+    res.render('404');
     return;
   }
 
-  res.send(login.errors);
 };
