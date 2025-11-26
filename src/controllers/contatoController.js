1e708485bb4e46d0c6b465ae2efa82c1fce9cc9b
Rodolfo Charles <rosdolfo@gmail.com>
2025-11-26 15:30:46 -0300

    Cadastrando contatos(create)

---
+++ src/controllers/contatoController.js
@@ -0,0 +1,40 @@
+const Contato = require('../models/ContatoModel');
+
+exports.index = (req, res) => {
+  res.render('contato', {
+    contato: {}
+  });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body);
+    await contato.register();
+
+    if (contato.errors.length > 0) {
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('/contato/index'));
+      return;
+    } else {
+      req.flash('success', 'Contato registrado com sucesso');
+      //req.session.save(() => res.redirect('/contato/index'));
+      req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+      return;
+    }
+
+  } catch (e) {
+    console.log(e);
+    this.errors.push('Erro ao registrar contato');
+    res.render('404');
+    return;
+  }
+};
+
+exports.editIndex = async function(req, res) {
+  if(!req.params.id) return res.render('404');
+
+  const contato = await Contato.buscaPorId(req.params.id);
+  if(!contato) return res.render('404');
+
+  res.render('contato', {contato});
+} 
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -21,3 +21,12 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash('errors', 'Você precisa se logar no sistema.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+  next();
+};
